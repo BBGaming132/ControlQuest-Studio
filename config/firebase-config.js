a267bab714677bc570bb2fb43ece086eeb62d403
BBGaming132 <75038638+BBGaming132@users.noreply.github.com>
2026-06-01 06:35:27 -0400

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -2,10 +2,10 @@
 // Leave enabled: false while testing locally. The app will still work in local demo mode.
 export const firebaseConfig = {
   enabled: false,
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "PASTE_SENDER_ID",
-  appId: "PASTE_APP_ID"
+  apiKey: "AIzaSyCRAC_UP6SP1pM8QjgurD6emk50d6YHC1I",
+  authDomain: "controlquest-studio-18ba7.firebaseapp.com",
+  projectId: "controlquest-studio-18ba7",
+  storageBucket: "controlquest-studio-18ba7.firebasestorage.app",
+  messagingSenderId: "1030371545997",
+  appId: "1:1030371545997:web:78e69e529542830dac63c4"
 };
