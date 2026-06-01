aaad711da00a85de1ad3b10919f285d565cbe47d
BBGaming132 <75038638+BBGaming132@users.noreply.github.com>
2026-06-01 06:46:45 -0400

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,7 +1,7 @@
 // Copy your Firebase web app config here after creating the free Firebase project.
 // Leave enabled: false while testing locally. The app will still work in local demo mode.
 export const firebaseConfig = {
-  enabled: false,
+  enabled: true,
   apiKey: "AIzaSyCRAC_UP6SP1pM8QjgurD6emk50d6YHC1I",
   authDomain: "controlquest-studio-18ba7.firebaseapp.com",
   projectId: "controlquest-studio-18ba7",
